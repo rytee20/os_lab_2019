3b23856f1e7f88cf407b94d578736ec00124c1ae
Ekaterina <94058290+rytee20@users.noreply.github.com>
2023-10-04 17:14:22 +0000

    дописан revert_string.c

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -2,6 +2,12 @@
 
 void RevertString(char *str)
 {
-	// your code here
+	int len = strlen(str);
+	for (int i = 0; i < len / 2; i++)
+	{
+		char temp = str[i];
+		str[i] = str[len - i - 1];
+		str[len - i - 1] = temp;
+	}
 }
 
